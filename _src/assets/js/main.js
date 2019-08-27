5b13903601378cfa30c58ea3bb148ed63da12734
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-08-27 17:47:21 +0200

    create api petition and show cards in the page

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
+//
 const startBtn = document.querySelector('.js-startBtn');
 const cardsInput = document.querySelectorAll('.js-form__radio');
-const endpoint = `https://raw.githubusercontent.com/Adalab/cards-data/master/:NUMERO.json`;
+const cardsList = document.querySelector('.js-cards-list');
 
 //DEFAULT GAME DIFFICULTY
 let cardsNumber = 4;
@@ -24,7 +25,33 @@ function chooseDifficulty (event) {
 
 //GAME FUNCTION WITH API
 function game () {
+  const endpoint = `https://raw.githubusercontent.com/Adalab/cards-data/master/${cardsNumber}.json`;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      let card = '';
+      for (const item of data) {
+        let cardUrlBack = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+        let cardNameBack = 'Cara trasera de la carta';
+        let cardUrlFront = item.image;
+        let cardName = item.name;
+        let cardPair = item.pair;
 
+        card += `<li>
+                  <img class="card__img hidden"
+                  src="${cardUrlFront}"
+                  alt="${cardName}"
+                  data-pair="${cardPair}">
+                  </img>
+                  <img class="card__img"
+                  src="${cardUrlBack}"
+                  alt="${cardNameBack}"
+                  data-pair="${cardPair}">
+                  </img>
+                </li>`;
+      }
+      cardsList.innerHTML = card;
+    });
 }
 
 //LISTENERS
