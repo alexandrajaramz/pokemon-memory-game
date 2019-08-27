44125f563d0c6fb64df0db3f00b7896cfcaf4978
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-08-27 15:54:57 +0200

    create funtions to choose game difficulty and set it to LS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('>> Ready :)');
+const startBtn = document.querySelector('.js-startBtn');
+const cardsInput = document.querySelectorAll('.js-form__radio');
+const endpoint = `https://raw.githubusercontent.com/Adalab/cards-data/master/:NUMERO.json`;
+
+//DEFAULT GAME DIFFICULTY
+let cardsNumber = 4;
+localStorage.setItem('gameDifficulty', cardsNumber);
+
+//CREATE LISTENER IN EVERY RADIUS OF THE ARRAY
+function selectCards () {
+  for (const input of cardsInput) {
+    input.addEventListener('click', chooseDifficulty);
+  }
+}
+selectCards();
+
+//SELECT CLICKED RADIUS AS DIFFICULTY
+function chooseDifficulty (event) {
+  cardsNumber = parseInt(event.currentTarget.value);
+  localStorage.setItem('gameDifficulty', cardsNumber);
+}
+
+//GAME FUNCTION WITH API
+function game () {
+
+}
+
+//LISTENERS
+startBtn.addEventListener('click', game);
